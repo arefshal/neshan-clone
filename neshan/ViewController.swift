fda727954599c72353fcae9ff879ab17e0928d93
Aref <arefshal@yahoo.com>
2024-09-05 17:53:23 +0330

    chore: initialize Xcode project

---
+++ neshan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  neshan
+//
+//  Created by Aref on 9/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
