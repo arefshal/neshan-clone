7879cf8efe5184672c65782fa6c01bdaf1b981a0
Aref <arefshal@yahoo.com>
2024-09-06 23:01:08 +0330

    refactor: update SearchResult and SearchResponse models with new fields

--- neshan/Model/SearchResult.swift
+++ neshan/Model/SearchResult.swift
@@ -10,5 +10,13 @@ import Foundation
 struct SearchResult: Codable {
     let title: String
     let address: String
+    let neighbourhood: String?
+    let region: String
+    let type: String
+    let category: String
     let location: Location
+}
+struct SearchResponse: Codable {
+    let count: Int
+    let items: [SearchResult]
 }
