c1c3ffd530f184488ba4b99576356701f904a4e0
Aref <arefshal@yahoo.com>
2024-09-07 10:07:29 +0330

    refactor: update RoutingService with logging, comments, and polyline decoding improvements

--- neshan/Services/RoutingService.swift
+++ neshan/Services/RoutingService.swift
@@ -15,7 +15,7 @@ class RoutingService {
     // MARK: - Properties
     
     /// The API key used for authenticating requests to the Neshan API.
-    private let apiKey = "service.4ce361741bbd4a2391b15c1004763139"
+    private let apiKey = "service.263c0097bf40475abe62c92e2a95ba66"
     
     // MARK: - Public Methods
     
@@ -27,7 +27,7 @@ class RoutingService {
     ///   - completion: A closure called with the resulting MKPolyline, or nil if routing failed.
     func getRoute(from origin: CLLocationCoordinate2D, to destination: CLLocationCoordinate2D, completion: @escaping (MKPolyline?) -> Void) {
         // Construct the base URL for the Neshan routing API
-        let baseUrl = "https://api.neshan.org/v4/direction/no-traffic"
+        let baseUrl = "https://api.neshan.org/v4/direction"
         let originString = "\(origin.latitude),\(origin.longitude)"
         let destinationString = "\(destination.latitude),\(destination.longitude)"
         
@@ -51,6 +51,10 @@ class RoutingService {
                 return
             }
             
+            
+            let responseString = String(data: data, encoding: .utf8)
+            print("Response: \(String(describing: responseString))")
+            
             // Attempt to decode the response
             do {
                 let decoder = JSONDecoder()
@@ -58,6 +62,14 @@ class RoutingService {
                 
                 // Extract and decode the polyline if available
                 if let overviewPolyline = routeResponse.routes.first?.overview_polyline.points {
+                   
+                    let decodedCoordinates = overviewPolyline.decodedPolyline()
+                    
+                    
+                    for coordinate in decodedCoordinates {
+                        print("Latitude: \(coordinate.latitude), Longitude: \(coordinate.longitude)")
+                    }
+                    
                     let polyline = self.decodePolyline(overviewPolyline)
                     DispatchQueue.main.async {
                         completion(polyline)
@@ -96,35 +108,39 @@ class RoutingService {
 extension String {
     func decodedPolyline() -> [(latitude: Double, longitude: Double)] {
         var coordinates: [(Double, Double)] = []
-        var index = 0
+        var index = self.startIndex
         var lat = 0.0
         var lon = 0.0
 
-        while index < self.count {
-            var result = 1
+        while index < self.endIndex {
+            var result = 0
             var shift = 0
             var b: Int
             repeat {
-                b = Int(self[self.index(self.startIndex, offsetBy: index)].asciiValue!) - 63
-                index += 1
-                result += (b & 0x1f) << shift
+                b = Int(self[index].asciiValue! - 63)
+                index = self.index(after: index)
+                result |= (b & 0x1f) << shift
                 shift += 5
             } while b >= 0x20
 
-            lat += Double((result & 1) != 0 ? ~(result >> 1) : (result >> 1))
-            
-            result = 1
+            let deltaLat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1))
+            lat += Double(deltaLat)
+
+            result = 0
             shift = 0
             repeat {
-                b = Int(self[self.index(self.startIndex, offsetBy: index)].asciiValue!) - 63
-                index += 1
-                result += (b & 0x1f) << shift
+                b = Int(self[index].asciiValue! - 63)
+                index = self.index(after: index)
+                result |= (b & 0x1f) << shift
                 shift += 5
             } while b >= 0x20
 
-            lon += Double((result & 1) != 0 ? ~(result >> 1) : (result >> 1))
-            
-            coordinates.append((lat * 1e-5, lon * 1e-5))
+            let deltaLon = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1))
+            lon += Double(deltaLon)
+
+            let latitude = lat * 1e-5
+            let longitude = lon * 1e-5
+            coordinates.append((latitude, longitude))
         }
 
         return coordinates
